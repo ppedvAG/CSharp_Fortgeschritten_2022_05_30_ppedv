735e2a52d6d4f49fd1ccdea850e221ab64752a42
ppedvLukasK <lukask@ppedv.de>
2022-05-30 15:26:10 +0200

    M005

---
+++ Generics/GenericConstraints.cs
@@ -0,0 +1,35 @@
+﻿namespace Generics;
+
+internal class GenericConstraints
+{
+	static void Main(string[] args)
+	{
+		//DataStore4<TestClass> ds4; //Nicht möglich
+	}
+
+	public class DataStore1<T> where T : class { } //Referenztyp erzwingen
+
+	public class DataStore2<T> where T : struct { } //Wertetyp erzwingen
+
+	public class DataStore3<T> where T : GenericConstraints { } //Bestimmte Vererbungshierarchie
+
+	public class DataStore4<T> where T : new() { } //Nur Typen die einen Default Konstruktor haben
+
+	public class DataStore5<T> where T : Enum { } //Nur Enum Werte
+
+	public class DataStore6<T> where T : Delegate { } //Nur Delegate
+
+	public class DataStore7<T> where T : unmanaged { } //Nur Code von außen
+
+	public class DataStore8<T1, T2> //Mehrere Constraints auf mehrere Generics
+		where T1 : class, new() //Mehrere Constraints auf T1
+		where T2 : struct
+	{
+
+	}
+}
+
+public class TestClass
+{
+	public TestClass(int i) { } //Default Konstruktor entfernt durch einen Konstruktor mit Parameter
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+
+namespace Generics
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> list = new(); //Generic: T wird nach unten übernommen
+			list.Add("Max"); //Zum Beispiel in Add(T) -> Add(string)
+
+			Dictionary<string, string> dictionary = new Dictionary<string, string>(); //Hier 2 Generics: TKey, TValue
+			dictionary.Add("Max", "Mustermann");
+
+			foreach (KeyValuePair<string, string> kv in dictionary) { }
+
+			DataStore<string> ds = new DataStore<string>();
+			ds.Add(1, "Test");
+			ds.DisplayDefault<long>();
+		}
+	}
+
+	public class DataStore<T> : IEnumerable<int>, IProgress<T> //int oder T nach unten weitergeben
+	{
+		private T[] data = new T[10]; //Array vom Typ T
+
+		public List<T> Data => data.ToList(); //Generic weitergeben in List
+
+		public void Add(int index, T item) //T mit Parameter
+		{
+			data[index] = item;
+		}
+
+		public T GetIndex(int index)
+		{
+			if (index < 0 || index >= data.Length)
+				return default(T); //default(T): Standardwert von T zurückgeben
+			return data[index];
+		}
+
+		public void DisplayDefault<MyType>() where MyType : struct //Weiteres Generic, bei Methode constrained
+		{
+			Console.WriteLine(default(MyType));
+		}
+
+		public IEnumerator<int> GetEnumerator() //int von Vererbung
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Report(T value) //T weitergeben von Progress<T>
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class DataStore2<T> : DataStore<T>
+	{
+
+	}
+}
