e757e8a73775c81c10ecce9804b6d66409046e1b
ppedvLukasK <lukask@ppedv.de>
2022-05-31 10:36:45 +0200

    Update SolarItem.cs

--- Generics/SolarSystemSolution/SolarItem.cs
+++ Generics/SolarSystemSolution/SolarItem.cs
@@ -1,17 +1,17 @@
 ﻿namespace SolarSystemSolution;
 
-//public class SolarItem
-//{
-//    public string Description { get; set; } 
-//    public SolarItemType Type { get; set; }
+public class SolarItem
+{
+	public string Description { get; set; }
+	public SolarItemType Type { get; set; }
 
-//    public SolarItem(string description, SolarItemType solarItemType)
-//    {
-//        Description = description;  
-//        Type = solarItemType;
-//    }
-//}
+	public SolarItem(string description, SolarItemType solarItemType)
+	{
+		Description = description;
+		Type = solarItemType;
+	}
+}
 
-record SolarItem(string Description, SolarItemType Type);
+//record SolarItem(string Description, SolarItemType Type);
 
 public enum SolarItemType { Star, Planet, Trabant }
