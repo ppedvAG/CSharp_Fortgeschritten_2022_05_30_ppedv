4cfeba6256f60c3de1d4ddd86b2b1c47940410e8
ppedvLukasK <lukask@ppedv.de>
2022-05-31 08:55:55 +0200

    M005 Lösung

---
+++ Generics/SolarSystemSolution/SolarItem.cs
@@ -0,0 +1,17 @@
+﻿namespace SolarSystemSolution;
+
+//public class SolarItem
+//{
+//    public string Description { get; set; } 
+//    public SolarItemType Type { get; set; }
+
+//    public SolarItem(string description, SolarItemType solarItemType)
+//    {
+//        Description = description;  
+//        Type = solarItemType;
+//    }
+//}
+
+record SolarItem(string Description, SolarItemType Type);
+
+public enum SolarItemType { Star, Planet, Trabant }
